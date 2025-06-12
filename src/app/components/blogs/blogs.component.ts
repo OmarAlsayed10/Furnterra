7d5209631edec8954ade3ffab9b4fd25f6ceb39d
omarxkira <k.omar.alsayed@gmail.com>
2025-06-12 05:29:26 +0300

    enchanced home page

---
+++ src/app/components/blogs/blogs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs',
+  imports: [],
+  templateUrl: './blogs.component.html',
+  styles: ``
+})
+export class BlogsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { HeroComponent } from "../../components/hero/hero.component";
 import { ProductsComponent } from '../../components/products/products.component';
 import { BestSellingComponent } from "../../components/best-selling/best-selling.component";
 import { SaleComponent } from "../../components/sale/sale.component";
+import { BlogsComponent } from "../../components/blogs/blogs.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [HeroComponent, ProductsComponent, BestSellingComponent, SaleComponent],
+  imports: [HeroComponent, ProductsComponent, BestSellingComponent, SaleComponent, BlogsComponent],
 })
 export class HomeComponent {
 
